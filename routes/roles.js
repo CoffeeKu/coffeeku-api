bf97ad6dcae587dd8ef5d02912d6918e08639da7
Teddy K <teddykoerniadi@gmail.com>
2024-07-23 22:01:42 +0700

    feat: Add migrations and models for Category and Menu
    The code changes include adding migrations and models for Category and Menu entities. This allows for the creation of corresponding database tables and defines the attributes for each entity. The Category model includes the attributes "name" and "description", while the Menu model includes "name", "description", and "price". These changes are necessary to support the functionality related to categories and menus in the application.
    
    Recent user commits:
    - refactor: Include Role information in user login response
    - chore: Update routes to include API versioning
    
    Recent repository commits:
    - refactor: Include Role information in user login response
    - chore: Update routes to include API versioning
    - init boilerplate project

--- routes/roles.js
+++ routes/roles.js
@@ -1,7 +1,7 @@
 
 const router = require("express").Router()
 const RoleController = require("../controllers/roleController");
-const { isLogin } = require("../middleware/authorize");
+const { isLogin } = require("../middleware/authentication");
 
 const roleController = new RoleController();
 
